9c613ff406e556905c0ba2cbdf82834e27fc900f
ken0426 <kenkaitixyou0426@gmail.com>
2020-11-25 19:03:01 +0900

    コントローラーの作成

--- db/migrate/20201125095458_create_contents.rb
+++ db/migrate/20201125095458_create_contents.rb
@@ -1,7 +1,8 @@
 class CreateContents < ActiveRecord::Migration[6.0]
   def change
     create_table :contents do |t|
-
+      t.string :title
+      t.text :contents
       t.timestamps
     end
   end
