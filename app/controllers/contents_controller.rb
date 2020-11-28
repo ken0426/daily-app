c3626a9699fbe741f4614223dd136060b9b6a616
ken0426 <kenkaitixyou0426@gmail.com>
2020-11-29 00:43:35 +0900

    新規投稿/編集/削除を実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,47 @@
 class ContentsController < ApplicationController
   def index
+    @content = Content.all.order("created_at DESC")
+  end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.new(content_params)
+    if @content.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @content = Content.find(params[:id])
+  end
+
+  def update
+    content = Content.find(params[:id])
+    content.update(content_params)
+    if content.save
+      redirect_to content_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    content = Content.find(params[:id])
+    content.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @content = Content.find(params[:id])
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(:title, :contents)
   end
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,4 @@
 class Content < ApplicationRecord
+  validates :title, presence: true
+  validates :contents, presence: true
 end
